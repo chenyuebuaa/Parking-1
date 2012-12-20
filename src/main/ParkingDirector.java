a4e54502ee502398f6214f8cb70d701d2ea04c38
wanling <fppling@gmail.com>
2012-12-20 22:58:30 +0800

    change

--- src/main/ParkingDirector.java
+++ src/main/ParkingDirector.java
@@ -1,10 +1,16 @@
-﻿package main;
+package main;
 
-
+/**
+ * Created with IntelliJ IDEA.
+ * User: software
+ * Date: 12-12-20
+ * Time: 下午10:56
+ * To change this template use File | Settings | File Templates.
+ */
 public class ParkingDirector {
 
-        public void report(Component c)
-        {
-            System.out.println(c.diplay(0)+"\n");
-        }
+    public void report(Component c)
+    {
+        System.out.println(c.diplay(0)+"\n");
+    }
 }
